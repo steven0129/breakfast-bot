466e44bb8da67a45151dd55bc4a054602d13e5cc
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-07-30 20:03:09 +0800

    change framework to koa

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const linebot = require('linebot')
-const express = require('express')
-const app = express()
-const bot = linebot({
-    channelId: process.env.CHANNEL_ID,
-    channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+const Koa = require('koa')
+const app = new Koa()
+const router = require('koa-router')()
+const logger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
+const json = require('koa-json')
+const bot = require('@line/bot-sdk')
+const botMiddleware = require('@line/bot-sdk').middleware
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+}
+
+router.post('/webhooks', async (ctx, next) => {
+    ctx.body = ctx.request.body.events
 })
 
-bot.on('message', (e) => {
-    if (e.message.type === 'text') {
-        let msg = e.message.text
-        e.reply(msg).then((data) => {
-            console.log(msg)
-        }).catch((error) => {
-            console.log('error')
-        })
-    }
-})
+app.use(logger())
+app.use(bodyParser())
+app.use(json())
+app.use(botMiddleware(config))
+app.use(router.routes())
 
-var linebotParser = bot.parser()
-app.post('/', linebotParser)
-
-const server = app.listen(process.env.PORT || 8080, () => {
-    let port = server.address().port;
-    console.log(`App now running on port ${port}`)
-})
+app.listen(process.env.PORT || 8080)
