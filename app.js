eb5f745bd9d86aa488f5fbe44c00167056097dd6
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-07-30 21:20:42 +0800

    add LUIS backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const linebot = require('linebot')
 const app = require('express')()
+const request = require('request-promise')
 const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
@@ -10,16 +11,15 @@ bot.on('message', (e) => {
     if (e.message.type === 'text') {
         console.log(e.message)
         let msg = e.message.text
-        e.reply(msg).then((data) => {
-            console.log(msg)
-        }).catch((error) => {
-            console.log('error')
-        })
+        request(process.env.LUIS_URL + msg)
+            .then((response) => {
+                e.reply(msg).catch((error) => console.log(error))
+            })
+            .catch((error) => console.log(error))
     }
 })
 
-var linebotParser = bot.parser()
-app.post('/webhook', linebotParser)
+app.post('/webhook', bot.parser())
 
 const server = app.listen(process.env.PORT || 8080, () => {
     let port = server.address().port;
