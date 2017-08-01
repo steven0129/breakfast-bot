a24109b67744aefda207a469b5e0b61a9b527887
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-08-01 23:48:14 +0800

    rule['詢問推薦早餐'] finish

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
-const linebot = require('linebot')
 const app = require('express')()
-const request = require('request-promise')
-const bot = linebot({
-    channelId: process.env.CHANNEL_ID,
-    channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-})
+const errorhandler = require('errorhandler')
+const diContainer = require('./lib/diContainer.js')
 
-bot.on('message', (e) => {
-    if (e.message.type === 'text') {
-        let msg = e.message.text
-        request(process.env.LUIS_URL + encodeURIComponent(msg))
-            .then((response) => {
-                response = JSON.parse(response)
-                switch (response.intents[0].intent) {
-                    case '打招呼':
-                        if (response.intents[0].score > 0.75) {
-                            let ans = ['您好', '哈囉', '您好, 非常高興為您服務', 'hello']
-                            let index = Math.floor(Math.random() * ans.length)
-                            e.reply(ans[index]).catch((error) => console.log(error))
-                        } else {
-                            e.reply('不好意思, 小哲只能了解早餐相關的事情').catch((error) => console.log(error))
-                        }
-                        break
-                    default:
-                        e.reply('不好意思, 小哲只能了解早餐相關的事情').catch((error) => console.log(error))
-                        break
-                }
-            })
-            .catch((error) => console.log(error))
-    }
-})
+diContainer.register('channelId', process.env.CHANNEL_ID)
+diContainer.register('channelSecret', process.env.CHANNEL_SECRET)
+diContainer.register('channelAccessToekn', process.env.CHANNEL_ACCESS_TOKEN)
+diContainer.register('dbName', 'menus')
+diContainer.register('dbUrl', process.env.MONGODB_URL)
+diContainer.register('greets', ['您好', '哈囉', '您好, 非常高興為您服務', 'hello'])
+diContainer.register('app', app)
+diContainer.factory('luis', require('./lib/luis.js'))
+diContainer.factory('botController', require('./lib/botController.js'))
+diContainer.factory('botService', require('./lib/botSerice.js'))
+diContainer.factory('botRule', require('./lib/botRule.js'))
 
-app.post('/webhook', bot.parser())
+const botController = diContainer.get('botController')
+
+app.use(errorhandler())
+app.post('/webhook', botController.parser)
 
 const server = app.listen(process.env.PORT || 8080, () => {
     let port = server.address().port
